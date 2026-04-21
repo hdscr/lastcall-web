be5afb15a47a3a16a914da61343539b6168dff19
hdscr <51625890+hdscr@users.noreply.github.com>
2026-04-21 16:40:13 +0200

    feat(home): add instruments db query to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import { createClient } from "@/lib/supabase/server";
+import { Suspense } from "react";
+
+async function InstrumentsData() {
+  const supabase = await createClient();
+  const { data: instruments } = await supabase.from("instruments").select();
+
+  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
+}
+
 export default function Home() {
-  return <h1>Jetzt funzt es gugus</h1>;
+  return (
+    <main>
+      <h1>Jetzt funzt es gugus</h1>
+      <Suspense fallback={<div>Loading instruments...</div>}>
+        <InstrumentsData />
+      </Suspense>
+    </main>
+  );
 }
